d683ee77f875a731c0bcbd931e459b64bfd38a4b
Abhinav <drabhinavrao2000@gmail.com>
2026-09-01 19:15:02 +0530

    new frontend

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://smartreviewsapp.netlify.app",
+        "https://smartreviews-mcjc.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
